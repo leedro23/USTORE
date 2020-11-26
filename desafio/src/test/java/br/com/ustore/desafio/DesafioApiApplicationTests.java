593f0ea88240fae5c22760be03e3c2983e8fbe50
Leonardo Felix <leonardofelixfelix@gmail.com>
2020-11-25 22:01:05 -0300

    Dependecias

---
+++ desafio/src/test/java/br/com/ustore/desafio/DesafioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ustore.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
